54de2e0f19662abcc287683da09a0061f0d422d7
Owain <owain.jones74@gmail.com>
2020-10-06 22:29:58 +0100

    update config comments

--- lua/xsgroups/_config/sh_config.lua
+++ lua/xsgroups/_config/sh_config.lua
@@ -1,4 +1,9 @@
--- Paid ranks
+-- Here you can register seconday usergroups.
+-- The first value is the group name, the 2nd is their power.
+
+-- Staff rank
 xSGroups.Core.RegisterGroup("event", 30)
+
+-- Paid rank
 xSGroups.Core.RegisterGroup("elite", 10)
 xSGroups.Core.RegisterGroup("vip", 10)
